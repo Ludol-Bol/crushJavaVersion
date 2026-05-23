10dd5cc2cf505c01634ce1a08e639bca5dec222e
dobro <dobrovanovaluda@gmail.com>
2026-05-23 12:19:46 +0500

    testPage

---
+++ src/main/java/com/crushVers/CrushVerseApp.java
@@ -0,0 +1,15 @@
+package com.crushVers;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.crushVers", "com.crushVers.login"})
+public class CrushVerseApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrushVerseApp.class, args);
+        System.out.println(" CrushVerseApp запущен! ");
+        System.out.println(" Откройте: http://localhost:8080 ");
+    }
+}
